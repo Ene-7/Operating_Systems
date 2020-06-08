bfdbb5e88cbc0779bea87351dad59ccf93e19542
Eneid <eneidpapa1@gmail.com>
2020-06-07 20:40:47 -0400

    Added Zombie.c

---
+++ zombie.c
@@ -0,0 +1,18 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main() 
+{
+	int processID = fork();
+	
+	if(processID == 0) {
+		printf("I am the child with PID [%d] and my parent has PPID [%d].\n", getpid(), getppid());
+		sleep(1);
+	}
+	
+	else {
+		printf("I am the parent and my id is [%d]\n", getpid());
+		sleep(30);
+	}
+    return 0;
+}
