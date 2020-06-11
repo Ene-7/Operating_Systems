438bb55919b01edcf9a91fec1f542dfbb4476713
Eneid <eneidpapa1@gmail.com>
2020-06-11 00:42:19 -0400

    Added more to Run() for Customer

--- Project/src/Customer.java
+++ Project/src/Customer.java
@@ -1,5 +1,4 @@
 
-
 public class Customer implements Runnable {
     private String name;
     private Thread CustomerThread;
@@ -9,7 +8,6 @@ public class Customer implements Runnable {
         setName("Customer_" + Num);
         this.CustomerThread = new Thread(this, name);
     }
-
 
     public void setName(String in){
         this.name = in;
@@ -27,17 +25,27 @@ public class Customer implements Runnable {
     @Override
     public void run() {
         try {
-            this.CustomerThread.sleep(Main.RandomTime(1,120));
+            Thread.sleep(Main.RandomTime(1,120));
         }
         catch (InterruptedException e) {
             e.printStackTrace();
         } // This will simulate an arrival time from 1 to 120 seconds (2 Minutes).
         msg("Has arrived at the stores' parking lot, and is now in the queue waiting to go in.");
         Main.CUSTOMER_QUEUE.add(this); // Customer Object added to the Queue Located in Main.
+
         //TODO: Busy Wait until store has space available to allow 6 customers inside to shop.
         // Busy Wait code here... Make sure they get in a FCFS order.
 
-        //TODO: Customer queue 6 at a time
+        while(Main.CUSTOMERS_SHOPPING.get() >= 6){
+          continue; // Although this is not necessary I've placed it to keep IntelliJ from complaining.
+          // Busy Wait if there are 6 people shopping in the store exit once there is space.
+        }
+
+        Customer ME = Main.CUSTOMER_QUEUE.peek(); // See who the very first one in line is.
+        Main.CUSTOMER_QUEUE.remove(); // Remove them from the Queue that they were waiting for outside.
+        Main.CUSTOMERS_SHOPPING.getAndIncrement(); // Add 1 to the Counter of shoppers to prevent others from coming in if there's 6 inside.
+
+        ME.msg("I'm finally inside and can shop. I better stay away from others, they could be sick!");
 
     }
 
--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
@@ -5,13 +6,15 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger; // Not sure If these two are needed...
 
 public class Main {
-
     private static Manager[] Managers;
     private static Employee[] Employees;
     private static Customer[] Customers;
     public static int NumCustomers;
+    public static int Store_Capacity = 6;
+    public static int NumSelf_Checkout = 4;
     public static long time = System.currentTimeMillis();
     public static Queue<Customer> CUSTOMER_QUEUE = new LinkedList<>();
+    public static AtomicInteger CUSTOMERS_SHOPPING = new AtomicInteger(0);
 
     public static void main(String[] args){
             try {
@@ -21,21 +24,33 @@ public class Main {
                 System.out.println("Invalid Input. Input Should Be An Integer Value. Please Reconfigure The Arguments And Try Again.");
             } // As requested, the number of Customers will be specified by an argument value.
 
-
         // Creates an array for 10 BabyGeese threads
         Managers = new Manager[1]; // review count
         Employees = new Employee[3]; //review count
         Customers = new Customer[NumCustomers];
 
         // Creates the threads
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < Managers.length; i++) {
             Managers[i] = new Manager(Integer.toString(i));
+        }
+        for(int i = 0; i < Employees.length; i++){
             Employees[i] = new Employee(Integer.toString(i));
+        }
+        for(int i = 0; i < Customers.length; i++){
             Customers[i] = new Customer(Integer.toString(i));
         }
-        // Starts the BabyGeese threads
-        for (int i= 0; i < 10; i++) {
-            Managers[i].start();
+
+        // Starts the threads
+        for (Manager manager : Managers) {
+            manager.start();
+        }
+
+        for (Employee employee : Employees) {
+            employee.start();
+        }
+
+        for (Customer customer : Customers) {
+            customer.start();
         }
     }
 
